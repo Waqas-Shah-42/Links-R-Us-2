d09ae61b1e3f242f1b07a9be553bc76ceca3f328
Waqas-Shah-42 <wshah052@uottawa.ca>
2022-09-05 15:50:38 +0500

    Added bleve

--- textindexer/store/memory/bleve.go
+++ textindexer/store/memory/bleve.go
@@ -4,11 +4,14 @@ import (
 	"sync"
 
 	"github.com/Waqas-Shah-42/Links-R-Us-2/textindexer/index"
+	"github.com/blevesearch/bleve"
 )
 
 type InMemoryBleveIndexer struct {
 	mu sync.RWMutex
 
 	docs map[string]*index.Document
+
+	idx bleve.Index
 
 }
